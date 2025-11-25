633e8bcf69230e5f5f78bd5a2842f2ceb69d84f4
Ester-Fr <sarafrankb@gmail.com>
2025-11-25 02:03:34 +0200

    Add function to find all increasing subarrays

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 1, 2};
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            List<Integer> current = new ArrayList<>();
+            current.add(arr[i]);
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > current.get(current.size() - 1)) {
+                    current.add(arr[j]);
+                } else {
+                    break;
+                }
+            }
+
+            if (current.size() > 1) {
+                result.add(current);
+            }
+        }
+
+        for (List<Integer> seq : result) {
+            System.out.println(seq);
+        }
+    }
 }
